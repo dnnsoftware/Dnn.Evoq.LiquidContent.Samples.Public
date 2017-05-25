c0776796aec8d28f7f3948f7c90e56639573a923
Kan Ma <kan.ma@dnnsoftware.com>
2017-05-25 16:33:40 -0700

    File cleanup

---
+++ Evoq.Modules/01-JobList.WebForm/Modules/Evoq.Modules.LiquidContentJobs/Components/Authorization/ScopeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Evoq.Microservices.Framework.Authorization;
+
+namespace Evoq.Modules.LiquidContentJobs.Components.Authorization
+{
+    public class ScopeManager : IScopeManager
+    {
+        private static readonly string[] DefaultScopes =
+        {
+            GetScopeIdentifier(Constants.ContentTypeResourceName, Constants.ReadAction),
+            GetScopeIdentifier(Constants.ContentItemResourceName, Constants.ReadAction)
+        };
+
+        private static string GetScopeIdentifier(string resourceName, string action)
+        {
+            return ScopeFormatHelper.FormatScopeIdentifier(resourceName, action);
+        }
+
+        public string GetScopeByRoles(string[] roles)
+        {
+            var scopes = new HashSet<string>();
+            foreach (var scope in DefaultScopes)
+            {
+                scopes.Add(scope);
+            }
+
+            return ScopeFormatHelper.JoinScopes(scopes.ToArray());
+        }
+    }
+}
---
+++ Evoq.Modules/01-JobList.WebForm/Website/DesktopModules/LiquidContentJobs/View.ascx
@@ -0,0 +1,3 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="View.ascx.cs" Inherits="Evoq.Modules.LiquidContentJobs.View" %>
+<div runat="server" ID="ScopeWrapper">
+</div>
