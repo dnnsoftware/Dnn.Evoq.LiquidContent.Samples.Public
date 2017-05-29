9dd19dd9821922ba79329d54544c11beda15b985
Kan Ma <kan.ma@dnnsoftware.com>
2017-05-29 16:41:59 -0700

    cleanup files

--- Evoq.Modules/02-JobList.AngularJS.Simple/Modules/Evoq.Modules.LiquidContentJobs/Components/Constants.cs
+++ Evoq.Modules/02-JobList.AngularJS.Simple/Modules/Evoq.Modules.LiquidContentJobs/Components/Constants.cs
@@ -3,23 +3,13 @@ namespace Evoq.Modules.LiquidContentJobs.Components
 {
     public class Constants
     {
-        internal const string InstallationPath = "DesktopModules/LiquidContentJobs";
-
-        internal const string SharedResources = "~/" + InstallationPath + "/App_LocalResources/SharedResources.resx";
-
         internal const string ApplicationId = "dnn-structured-content";
 
         internal const string ApplicationName = "Structured Content";
 
-        public const string PackageName = "LiquidContentJobs";
-
         internal const string ContentTypeResourceName = "sc-type";
 
         internal const string ContentItemResourceName = "sc-item";
-
-        internal const string ServiceEndPointWebConfigKey = "Services.Endpoints.StructuredContent.Web";
-
-        internal const string ServiceEndPointApiConfigKey = "Services.Endpoints.StructuredContent.Api";
 
         internal const string ReadAction = "read";
 
